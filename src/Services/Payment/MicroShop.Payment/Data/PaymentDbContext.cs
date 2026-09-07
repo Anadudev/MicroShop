a639ead601f21b5c0d1af751581f4498a728b3de
Anadudev <anadudev@gmail.com>
2026-09-08 00:49:24 +0100

    feat: Add transactional outbox implementation with InboxState and OutboxMessage tables in Order service

--- src/Services/Payment/MicroShop.Payment/Data/PaymentDbContext.cs
+++ src/Services/Payment/MicroShop.Payment/Data/PaymentDbContext.cs
@@ -5,7 +5,7 @@ namespace MicroShop.Payment.Data;
 
 public class PaymentDbContext(DbContextOptions<PaymentDbContext> options) : DbContext(options)
 {
-    public DbSet<Models.Payment> Payments => Set<Models.Payment>();
+    public DbSet<Models.Payment> Payments => Set<Models.Payment>(); 
     public DbSet<Models.ProcessedMessage> ProcessedMessages => Set<Models.ProcessedMessage>();
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
